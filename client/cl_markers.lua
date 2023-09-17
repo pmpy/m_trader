63f1759995689279bb48596cf40551e32dbb4e8b
Manny <50792377+pmpy@users.noreply.github.com>
2023-09-18 01:58:57 +1000

    fix random error

--- client/cl_markers.lua
+++ client/cl_markers.lua
@@ -51,17 +51,19 @@ end
 FetchInsideMarker = function()
     local playerCoords = GetEntityCoords(PlayerPedId())
 
-    for _, traderData in pairs(AccessibleTraders) do
-        local distance = #(playerCoords - traderData.marker.location)
+    if AccessibleTraders then
+        for _, traderData in pairs(AccessibleTraders) do
+            local distance = #(playerCoords - traderData.marker.location)
 
-        if insideMarker and insideMarker.prompt.data:isEnabled() and IsMenuOpen() then
-            insideMarker.prompt.data:setEnabledAndVisible(false)
-        elseif distance < traderData.marker.size then
-            insideMarker = traderData
-            return traderData
-        elseif insideMarker and traderData == insideMarker then
-            insideMarker.prompt.data:setEnabledAndVisible(false)
-            insideMarker = nil
+            if insideMarker and insideMarker.prompt.data:isEnabled() and IsMenuOpen() then
+                insideMarker.prompt.data:setEnabledAndVisible(false)
+            elseif distance < traderData.marker.size then
+                insideMarker = traderData
+                return traderData
+            elseif insideMarker and traderData == insideMarker then
+                insideMarker.prompt.data:setEnabledAndVisible(false)
+                insideMarker = nil
+            end
         end
     end
 
@@ -75,15 +77,17 @@ end
 DrawDisplayMarkers = function()
     local playerCoords = GetEntityCoords(PlayerPedId())
 
-    for _, traderData in pairs(AccessibleTraders) do
-        local distance = #(playerCoords - traderData.marker.location)
+    if AccessibleTraders then
+        for _, traderData in pairs(AccessibleTraders) do
+            local distance = #(playerCoords - traderData.marker.location)
 
-        if distance < traderData.marker.displayDistance then
-            local markerData = traderData.marker
-            local location = markerData.location
-            local colour = markerData.colour
+            if distance < traderData.marker.displayDistance then
+                local markerData = traderData.marker
+                local location = markerData.location
+                local colour = markerData.colour
 
-            Citizen.InvokeNative(0x2A32FAA57B937173, 0x07DCE236, location.x, location.y, location.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, markerData.size, markerData.size, markerData.size, colour.r, colour.g, colour.b, 100, false, true, 2, false, false, false, false)
+                Citizen.InvokeNative(0x2A32FAA57B937173, 0x07DCE236, location.x, location.y, location.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, markerData.size, markerData.size, markerData.size, colour.r, colour.g, colour.b, 100, false, true, 2, false, false, false, false)
+            end
         end
     end
 end
